c2322904c1571fff46c3b573a7e400a080e8149b
Hassan <70138791+Hassan-Mohamed@users.noreply.github.com>
2022-12-07 23:53:53 +0300

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,8 +3,8 @@
 # Python program that:
 # demonstrates how to return the number of keys in a dictionary
 #
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
+# (C) 2022 Hassan Mohamed, Nairobi, Kenya
+# email haska0491434@gmail.com
 # -----------------------------------------------------------
 
 
