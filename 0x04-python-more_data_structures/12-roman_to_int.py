8a795ee89c2b2e5e507a15f2d0db9c7813984343
Hassan <70138791+Hassan-Mohamed@users.noreply.github.com>
2022-12-07 23:46:04 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@
 # Python program that:
 # demonstrates how to convert a Roman numeral to an integer
 #
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
+# (C) 2022 Hassan Mohamed, Nairobi, Kenya
+# email haska0491434@gmail.com
 # -----------------------------------------------------------
 
 
